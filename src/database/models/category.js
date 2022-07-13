d25f294b864b4f1454660e7f9af4e56772482d66
cassiusbessa <cassiusbessa@gmail.com>
2022-07-13 18:51:11 -0300

    feat(database): :art: criado o model tabela Categories

---
+++ src/database/models/category.js
@@ -0,0 +1,19 @@
+'use strict';
+//JSdocs
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const createCategoryModel = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+  }, {
+    underscored: true,
+    tableName: 'Categories'
+  })
+  return Category;
+};
+
+module.exports = createCategoryModel
+
